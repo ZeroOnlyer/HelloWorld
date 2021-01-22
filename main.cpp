48683d6baedda686dc6d6b56dc49b797163f4294
Lx <478684715@qq.com>
2021-01-22 21:42:54 +0800

    创建main.cpp，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+
+}
+int main()
+{
+	int arr[] = { 12,4,5,54,87,32 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	sort(arr, size);
+	return 0;
+}
